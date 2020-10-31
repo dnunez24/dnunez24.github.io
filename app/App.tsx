c71ac4feffdf336aae96f76435c936b7d66c388c
Dave Nunez <david.nunez@alumni.stanford.edu>
2020-10-31 15:57:09 -0700

    Clean up App component

--- app/App.tsx
+++ app/App.tsx
@@ -1,21 +1,8 @@
-import React from 'react';
-import type { FC } from 'react';
-// import { ExperimentsProvider } from 'common/experimentation';
-// import { ErrorHandlerProvider } from 'common/errors';
-// import { FetchProvider } from 'common/data-fetching';
-// import { AppStateProvider } from './AppStateProvider';
-import { AppThemeProvider } from './AppThemeProvider';
+import type { FC } from "react";
+import { AppThemeProvider } from "./AppThemeProvider";
 
 const App: FC = ({ children }) => (
-  // <AppStateProvider>
-  //   <ErrorHandlerProvider>
-  //     <ExperimentsProvider>
-  //       <FetchProvider>
   <AppThemeProvider>{children}</AppThemeProvider>
-  //       </FetchProvider>
-  //     </ExperimentsProvider>
-  //   </ErrorHandlerProvider>
-  // </AppStateProvider>
 );
 
 export { App };
